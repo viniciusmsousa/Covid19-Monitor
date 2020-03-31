f8cba6aec58ecfb6ce02b0b8254faf2bc1b6e34d
vini <vinicius.sousa@aquare.la>
2020-03-30 21:11:01 -0300

    [infra] environment setup

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,43 +15,45 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "thinkr" )
-
+usethis::use_package("dplyr")
+usethis::use_package("tidyr")
+usethis::use_package("shiny")
+usethis::use_package("ggplot2")
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module( name = "name_of_module1" ) # Name of the module
-golem::add_module( name = "name_of_module2" ) # Name of the module
+
 
 ## Add helper functions ----
 ## Creates ftc_* and utils_*
-golem::add_fct( "helpers" ) 
-golem::add_utils( "helpers" )
+#golem::add_fct( "helpers" ) 
+#golem::add_utils( "helpers" )
 
 ## External resources
 ## Creates .js and .css files at inst/app/www
-golem::add_js_file( "script" )
-golem::add_js_handler( "handlers" )
-golem::add_css_file( "custom" )
+#golem::add_js_file( "script" )
+#golem::add_js_handler( "handlers" )
+#golem::add_css_file( "custom" )
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw( name = "my_dataset", open = FALSE ) 
+#usethis::use_data_raw( name = "my_dataset", open = FALSE ) 
 
 ## Tests ----
 ## Add one line by test you want to create
-usethis::use_test( "app" )
+#usethis::use_test( "app" )
 
 # Documentation
 
 ## Vignette ----
-usethis::use_vignette("covid19")
-devtools::build_vignettes()
+#usethis::use_vignette("covid19")
+#devtools::build_vignettes()
 
 ## Code coverage ----
 ## (You'll need GitHub there)
-usethis::use_github()
-usethis::use_travis()
-usethis::use_appveyor()
+#usethis::use_github()
+#usethis::use_travis()
+#usethis::use_appveyor()
 
 # You're now set! ----
 # go to dev/03_deploy.R
